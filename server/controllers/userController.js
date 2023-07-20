6ea29fae609d1c7f56aca3cbb1a1fcfbf10a4b45
Colin Silvers <silversc1998@gmail.com>
2023-07-20 14:57:20 -0400

    auth progress

---
+++ server/controllers/userController.js
@@ -0,0 +1,66 @@
+const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../models/userModel');
+const e = require('express');
+
+const signupUser = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
+  if(!username || !email || !password) {
+    res.status(400);
+    throw new Error("Missing field");
+  }
+  const userAvaialable = await User.findOne(email);
+  if(userAvaialable) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+  //Hashed password
+  const hashedPassword = await bcrypt.hash(password, 10);
+  console.log("Hashed Password: ", hashedPassword);
+  const user = await User.create({
+    username,
+    email,
+    password: hashedPassword,
+  });
+
+  console.log(`User created ${user}`);
+  if(user) {
+    res.status(201).json({_id: user.id, email: user.email});
+  } else {
+    res.status(400);
+    throw new Error("User not valid");
+  }
+});
+
+const loginUser = asyncHandler (async (req, res) => {
+  const { email, password } = req.body;
+  if(!email || !password) {
+    res.status(400);
+    throw new Error("Missing field");
+  }
+  const user = await User.findOne({ email });
+  //compare password with hashed password
+  if(user && (await bcrypt.compare(password, user.password))) {
+    const accessToken = jwt.sign({
+      user: {
+        username: user.username,
+        email: user.email,
+        id: user.id,
+      },
+    }, 
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: "1m"}
+   );
+    res.status(200).json({ accessToken });
+  } else {
+    res.status(401);
+    throw new Error("email or password is not valid");
+  }
+});
+
+const getUser = asyncHandler (async (req, res) => {
+  res.json({message: "User"});
+});
+
+module.exports = { signupUser, loginUser, getUser }
