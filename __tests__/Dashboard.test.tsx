fa1439ecc4e9368f738740f03dbff7cafefe0bf1
paulkimofficial <paulkim0209@gmail.com>
2023-08-01 23:51:00 -0400

    jest config

---
+++ __tests__/Dashboard.test.tsx
@@ -0,0 +1,12 @@
+// import { render, screen, act } from '@testing-library/react';
+// import Dashboard from '../client/components/Dashboard';
+// import React from 'react';
+
+// test('renders application name', () => {
+//   act(() => {
+//     render(<Dashboard />);
+//   });
+
+//   const appName = screen.getByText(/kafka nightowl/i);
+//   expect(appName).toBeInTheDocument();
+// });
