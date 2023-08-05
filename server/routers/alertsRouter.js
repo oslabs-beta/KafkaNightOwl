b61d1dc16ff8539f27bf222e4e1569357880611e
Jeremy Holland <jpholland05@gmail.com>
2023-08-05 11:06:56 -0400

    changed import statement to require

--- server/routers/alertsRouter.js
+++ server/routers/alertsRouter.js
@@ -1,9 +1,9 @@
-import { getAlerts } from "../controllers/alertsController";
+const { getAlerts } = require('../controllers/alertsController');
 const express = require('express');
 const alertsRouter = express.Router();
 
 alertsRouter.get('/promAlerts', getAlerts, (req, res) => {
-  return res.status(200).json(res.locals.alerts);
+	return res.status(200).json(res.locals.alerts);
 });
 
 module.exports = alertsRouter;
