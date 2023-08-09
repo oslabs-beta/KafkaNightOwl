d6eaefb71a3f9ccf6ef350a672c4fbf498466c90
Jeremy Holland <jpholland05@gmail.com>
2023-08-09 11:33:49 -0400

    deployment ready

--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const express = require('express');
 const app = express();
 const host = '0.0.0.0';
 const port = process.env.PORT || 5050;
@@ -10,7 +9,6 @@ const userRouter = require('./routers/userRoutes.js');
 const alertsRouter = require('./routers/alertsRouter.js');
 const connectDb = require('./config/dbConnection');
 
-app.use(cors());
 app.use(cors());
 app.use(express.json());
 connectDb();
@@ -23,11 +21,8 @@ app.use('/build', express.static(path.join(__dirname, '../build')));
 
 app.use('/user', userRouter);
 app.use('/jmx', metricsRouter);
-app.use('/jmx', metricsRouter);
 app.use('/alerts', alertsRouter);
 
-app.get('/', (req, res) => {
-	return res.status(200).sendFile(path.join(__dirname, '../build/index.html'));
 app.get('/', (req, res) => {
 	return res.status(200).sendFile(path.join(__dirname, '../build/index.html'));
 });
@@ -35,21 +30,10 @@ app.get('/', (req, res) => {
 // Catch-all route
 app.get('*', (req, res) => {
 	res.sendStatus(404);
-app.get('*', (req, res) => {
-	res.sendStatus(404);
 });
 
 // Global error handler asd
 app.use((err, req, res, next) => {
-	console.log(err);
-	const defaultErr = {
-		log: 'Express error handler caught unknown middleware error',
-		status: 400,
-		message: { err: 'An error occurred' },
-	};
-	const errorObj = Object.assign({}, defaultErr, err);
-	console.log(errorObj.log);
-	return res.status(errorObj.status).json(errorObj.message);
 	console.log(err);
 	const defaultErr = {
 		log: 'Express error handler caught unknown middleware error',
